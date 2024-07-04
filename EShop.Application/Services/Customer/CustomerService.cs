5f18c6092cd6e48f8bdce097586cebfbfd1e2aaf
alirezasiavashi <Alirezasiavashi981@gmail.com>
2024-07-05 00:40:49 +0330

    dev customer service

---
+++ EShop.Application/Services/Customer/CustomerService.cs
@@ -0,0 +1,14 @@
+﻿using EShop.Infrastructure.Repositories.Implementations;
+
+namespace EShop.Application.Services.Customer
+{
+    public class CustomerService
+    {
+        private readonly CustomerRepository _repository;
+
+        public CustomerService(CustomerRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
